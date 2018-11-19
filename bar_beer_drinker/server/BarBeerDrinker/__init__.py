12bf8f9c394ddea0ec23a0dc4aa868add5f6a441
Andy Val <andyval4@gmail.com>
2018-11-18 19:47:12 -0500

    redo
    everything added

--- bar_beer_drinker/server/BarBeerDrinker/__init__.py
+++ bar_beer_drinker/server/BarBeerDrinker/__init__.py
@@ -8,7 +8,15 @@ import json
 from BarBeerDrinker import database
 
 app = Flask(__name__)
-@app.route('/api/ratings', methods=["GET"])
-def get_ratings():
-    return jsonify(database.get_ratings())
+@app.route('/', methods=["GET"])
+def get_total():
+    return jsonify(database.get_total())
 
+@app.route("/api/bars",methods=["GET"])
+def get_bars():
+    return jsonify(database.get_bars())
+@app.route("/api/bars/<name>",methods=["GET"])
+def find_bars(name):
+    try:
+        if name is None:
+            return
--- bar_beer_drinker/server/BarBeerDrinker/database.py
+++ bar_beer_drinker/server/BarBeerDrinker/database.py
@@ -5,7 +5,11 @@ from BarBeerDrinker import config
 
 engine = create_engine(config.database_uri)
 
-def get_ratings():
+def get_total():
     with engine.connect() as con:
         rs = con.execute("SELECT * from bars")
+        return [dict(row) for row in rs]
+def get_bars():
+    with engine.connect() as con:
+        rs = con.execute("SELECT name from bars")
         return [dict(row) for row in rs]
